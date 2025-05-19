1e8343cbfee2e585c5b780d168153b74767c7f02
Dorian Koch <62394594+dorian-K@users.noreply.github.com>
2025-05-19 09:43:31 +0200

    fix ssh agent

--- borg_bk.sh
+++ borg_bk.sh
@@ -7,33 +7,40 @@ if ! ping -c 1 "$HOST" > /dev/null 2>&1; then
     exit 1
 fi
 
-if [[ $(hostname) == "rasp-new" ]]; then
-   eval $(ssh-agent -s)
-   ssh-add ~/.ssh/id_dorkframe_better
-fi
-
 # Setting this, so the repo does not need to be given on the commandline:
 export BORG_REPO=borg@${HOST}:/mnt/p1/backups
-
-# See the section "Passphrase notes" for more infos.
-# export BORG_PASSPHRASE='XYZl0ngandsecurepa_55_phrasea&&123'
-
-#export BORG_PASSCOMMAND='age --decrypt -i /home/dork/.ssh/id_dorkframe /home/dork/.ssh/borg_keyphrase'
 
 # some helpers and error handling:
 info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
 trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
 
 if [ -f ~/.ssh/id_dorkframe ]; then
-    export BORG_PASSPHRASE=$(age --decrypt -i ~/.ssh/id_dorkframe ~/.ssh/borg_keyphrase)
+    KEYFILE=~/.ssh/id_dorkframe
 elif [ -f ~/.ssh/id_dorkframe_better ]; then
-    export BORG_PASSPHRASE=$(age --decrypt -i ~/.ssh/id_dorkframe_better ~/.ssh/borg_keyphrase)
+    KEYFILE=~/.ssh/id_dorkframe_better
 elif [ -f ~/.ssh/id_ed25519 ]; then
-    export BORG_PASSPHRASE=$(age --decrypt -i ~/.ssh/id_ed25519 ~/.ssh/borg_keyphrase)
+    KEYFILE=~/.ssh/id_ed25519
 else
     echo "Neither id_dorkframe nor id_dorkframe_better nor id_ed25519 exists. Exiting."
     exit 1
 fi
+export BORG_PASSPHRASE=$(age --decrypt -i $KEYFILE ~/.ssh/borg_keyphrase)
+
+if ! ssh-add -l >/dev/null 2>&1; then
+    eval $(ssh-agent -s)
+fi
+
+# Get the fingerprint of the public key
+KEY_FP=$(ssh-keygen -lf "${KEYFILE}.pub" | awk '{print $2}')
+
+# Check if this fingerprint is in the agent
+if ssh-add -l 2>/dev/null | grep -q "$KEY_FP"; then
+    echo "Key $KEYFILE is loaded in ssh-agent."
+else
+    echo "Key $KEYFILE is NOT loaded in ssh-agent."
+    ssh-add $KEYFILE
+fi
+
 
 info "Starting backup"
 
