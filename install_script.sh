827e1e4d43dcb9bb878b3487ca09322726534be1
Dorian Koch <62394594+dorian-K@users.noreply.github.com>
2025-07-31 08:53:40 +0200

    More

--- install_script.sh
+++ install_script.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 sudo pacman -Syu --noconfirm
 
 sudo pacman -S --needed --noconfirm zoxide fzf ripgrep hyprland hyprpaper \
-  waybar kitty hypridle hyprlock hyprpolkitagent wofi brightnessctl wireplumber pavucontrol
+  waybar kitty hypridle hyprlock hyprpolkitagent wofi brightnessctl wireplumber pavucontrol hyprshot
 yay -S --needed --noconfirm mako 
