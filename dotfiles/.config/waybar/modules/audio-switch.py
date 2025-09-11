69d81c7420cec8aa28119aeaa1a3b4465ee8987a
Dorian Koch <62394594+dorian-K@users.noreply.github.com>
2025-09-11 11:26:06 +0200

    Fix audio switch

--- dotfiles/.config/waybar/modules/audio-switch.py
+++ dotfiles/.config/waybar/modules/audio-switch.py
@@ -8,6 +8,7 @@ SKIP_SINK_NAMES = [
     "Easy Effects Sink",
     "",
     "Ellesmere HDMI Audio",
+    "HDA NVidia Digital Stereo (HDMI)"
 ]  # List of devices to skip, leave empty for no filtering
 ENABLE_NOTIFICATIONS = True  # Set to False to disable notifications
 
