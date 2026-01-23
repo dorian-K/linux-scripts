07b8ea2eea6eb68d2105eac7bdd26bdaf6634a6a
Dorian Koch <62394594+dorian-K@users.noreply.github.com>
2026-01-23 22:38:22 +0100

    more

--- install_script.sh
+++ install_script.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 sudo pacman -Syu --noconfirm
 
-sudo pacman -S --needed --noconfirm zoxide fzf ripgrep hyprland hyprpaper \
+sudo pacman -S --needed --noconfirm zoxide fzf ripgrep hyprland hyprpaper hyprlauncher \
   waybar kitty hypridle hyprlock hyprpolkitagent wofi brightnessctl wireplumber pavucontrol hyprshot \
   gnome-keyring network-manager-applet
 yay -S --needed --noconfirm mako 
