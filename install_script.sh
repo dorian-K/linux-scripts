1f4d9da118bf4680d4c7c243741139b9eaa84ef9
Dorian Koch <62394594+dorian-K@users.noreply.github.com>
2026-06-19 15:32:25 +0200

    more

--- install_script.sh
+++ install_script.sh
@@ -6,7 +6,7 @@ sudo pacman -Syu --noconfirm
 
 sudo pacman -S --needed --noconfirm zoxide fzf ripgrep hyprland hyprpaper hyprlauncher \
   waybar kitty hypridle hyprlock hyprpolkitagent wofi brightnessctl wireplumber pavucontrol hyprshot \
-  gnome-keyring network-manager-applet bc dunst
+  gnome-keyring network-manager-applet bc dunst hyprpicker
 
 install -Dm755 misc/aur-llm-review ~/.local/bin/aur-llm-review
 
